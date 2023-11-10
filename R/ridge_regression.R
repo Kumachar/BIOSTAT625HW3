bfc32a1bdd8cffa7c460e679f2db11ced59f15f2
unknown <1019190564@qq.com>
2023-11-10 14:32:35 -0500

    Add function ridge_regression

---
+++ R/ridge_regression.R
@@ -0,0 +1,19 @@
+#'Ridge regression
+#'
+#'This function is designed to obtain the coefficient b from ridge regression \deqn{Y = X\beta + \lambda*I}
+#'
+#'@param x is a matrix of input features, each row represents an observation
+#'@param y is a matrix of output labels, each row represents an observation
+#'
+#'@return the parameters \beta for ridge regression
+#'
+#'@examples
+#'square(3)
+#'
+#'@export
+#'
+
+ridge_regression<-function(y,X,k=0){
+  return('s')
+}
+
