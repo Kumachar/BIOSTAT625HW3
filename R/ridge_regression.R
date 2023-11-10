648919c1912f8318a79839b5f0f26f55316db6e9
unknown <1019190564@qq.com>
2023-11-10 14:56:23 -0500

    Update ridge_regression and its documentaion

--- R/ridge_regression.R
+++ R/ridge_regression.R
@@ -1,11 +1,17 @@
-#'Ridge regression
+#'Ridge Regression
 #'
-#'This function is designed to obtain the coefficient b from ridge regression \deqn{Y = X\beta + \lambda*I}
+#'This function is designed to obtain the coefficient b from ridge regression
+#'\deqn{Y = X\beta + \lambda I + e}
+#'where \eqn{\lambda} served as a predetermined hyperparamter to shift the diagonals of moment matrix.
+#'More information about this method could be found in references
+#'@references
+#'Smith, J. (2020). *The Comprehensive Guide to Circle Geometry*. Geometry Press.
 #'
 #'@param x is a matrix of input features, each row represents an observation
 #'@param y is a matrix of output labels, each row represents an observation
+#'@param k is a predetermined adjusting number. When k=0, model becomes linear regression
 #'
-#'@return the parameters \beta for ridge regression
+#'@return the matrix of parameters \eqn{\beta} for ridge regression
 #'
 #'@examples
 #'square(3)
