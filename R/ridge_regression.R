6c7c5b4717753f17e6e7cea7c40b9f977db91077
unknown <1019190564@qq.com>
2023-11-10 15:11:56 -0500

    Update ridge_regression and its documentaion

--- R/ridge_regression.R
+++ R/ridge_regression.R
@@ -1,11 +1,12 @@
 #'Ridge Regression
 #'
 #'This function is designed to obtain the coefficient b from ridge regression
-#'\deqn{Y = X\beta + \lambda I + e}
+#'\deqn{Y = X\beta + e}
+#'and minimize \eqn{||Y-X\beta||^2+\lambda||\beta||},
 #'where \eqn{\lambda} served as a predetermined hyperparamter to shift the diagonals of moment matrix.
-#'More information about this method could be found in references
+#'More information about this method could be found in references.
 #'@references
-#'Smith, J. (2020). *The Comprehensive Guide to Circle Geometry*. Geometry Press.
+#'Wikipedia contributors. (n.d.). Ridge regression. In Wikipedia. Retrieved [November, 11th, 2023], from https://en.wikipedia.org/wiki/Ridge_regression
 #'
 #'@param x is a matrix of input features, each row represents an observation
 #'@param y is a matrix of output labels, each row represents an observation
@@ -20,6 +21,7 @@
 #'
 
 ridge_regression<-function(y,X,k=0){
-  return('s')
+  #beta_hat =
+  return(solve(t(X)*X+k*diag(x=1,nrow=nrow(X)))*y)
 }
 
